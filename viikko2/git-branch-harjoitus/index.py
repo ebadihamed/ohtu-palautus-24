58a3239158d11758d24083629aec26f7b0deca7d
ebadihamed <ebadihamed@gmail.com>
2024-11-11 14:20:10 +0200

    Tulo added

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -3,6 +3,7 @@
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo
 
 logger("Aloitetaan")
 
@@ -11,6 +12,6 @@ y = int(input("luku 2: "))
 
 print(f"Lukuejen {x} + {y} summa on {summa(x, y)}")
 print(f"Lukujen {x} - {y} erotus on {erotus(x, y)}")
-
+print(f"Lukujen {x} * {y} tulo on {tulo(x, y)}")
 logger("Lopetetaan")
 print("goodbye!") # Lisäys bugikorjaus-branchissa
